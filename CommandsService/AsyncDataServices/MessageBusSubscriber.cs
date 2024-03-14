945753d4bbedcefb19c62a5c914b1690a5ae7dfd
V-Nilsson <89032509+V-Nilsson@users.noreply.github.com>
2024-03-14 11:18:44 +0100

    Setup subscriber for messagebus

---
+++ CommandsService/AsyncDataServices/MessageBusSubscriber.cs
@@ -0,0 +1,84 @@
+﻿
+using CommandsService.EventProcessing;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Serilog;
+using System.Text;
+using System.Threading.Channels;
+
+namespace CommandsService.AsyncDataServices;
+
+public class MessageBusSubscriber : BackgroundService
+{
+    private readonly IConfiguration configuration;
+    private readonly IEventProcessor eventProcessor;
+    private IConnection connection;
+    private IModel channel;
+    private string queueName;
+
+    public MessageBusSubscriber(IConfiguration configuration, IEventProcessor eventProcessor)
+    {
+        this.configuration = configuration;
+        this.eventProcessor = eventProcessor;
+
+        InitializeRabbitMq();
+    }
+
+    private void InitializeRabbitMq()
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = configuration["RabbitMqHost"],
+            Port = int.Parse(configuration["RabbitMqPort"]),
+        };
+
+        connection = factory.CreateConnection();
+        channel = connection.CreateModel();
+        channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+        queueName = channel.QueueDeclare().QueueName;
+        channel.QueueBind(
+            queue: queueName,
+            exchange: "trigger",
+            routingKey: "");
+
+        Log.Information("--> Listening on the Message Bus");
+
+        connection.ConnectionShutdown += RabbitMq_ConnectionShutdown;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        stoppingToken.ThrowIfCancellationRequested();
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (ModuleHandle, ea) =>
+        {
+            Log.Information("--> Event Received");
+            var body = ea.Body;
+            var notificationMessage = Encoding.UTF8.GetString(body.ToArray());
+
+            eventProcessor.ProcessEvent(notificationMessage);
+        };
+
+        channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+        return Task.CompletedTask;
+    }
+
+    private void RabbitMq_ConnectionShutdown(object sender, ShutdownEventArgs e)
+    {
+        Log.Information("--> Connection Shutdown");
+    }
+
+    public override void Dispose()
+    {
+        if (channel.IsOpen)
+        {
+            channel.Close();
+            connection.Close();
+        }
+
+        base.Dispose();
+    }
+}
--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,3 +1,4 @@
+using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseInMemo
 builder.Services.AddScoped<ICommandRepository, CommandRepository>();
 builder.Services.AddControllers();
 
+builder.Services.AddHostedService<MessageBusSubscriber>();
 builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
